2963013176133e1cbd6b21b502a4eaf85ce4dd1d
Desire <1580364749@qq.com>
2024-05-19 22:00:06 +0800

    fist commit

--- GenerateData.py
+++ GenerateData.py
@@ -2,12 +2,29 @@ import random
 import math
 import json
 import os
+import numpy as np
+from scipy.stats import gaussian_kde
 # from math import sqrt, log, pi, cos, sin
+
+
+import shortuuid
+import uuid
+def new_uuid(length=None):
+    if length is None:
+        return str(uuid.uuid1())
+    else:
+        return str(shortuuid.ShortUUID().random(length=length))
 
 daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 daysOfHalfMonth = [15, 16, 14, 14, 15, 16, 15, 15, 15, 16, 15, 15, 15, 16, 15, 16, 15, 15, 15, 16, 15, 15, 15, 16]
 daysOfQuarterMonth=[7,8,8,8,7,7,7,7,7,8,8,8,7,8,7,8,7,8,8,8,7,8,7,8,7,8,8,8,7,8,8,8,7,8,7,8,7,8,8,8,7,8,7,8,7,8,8,8]
 peak_string = ["zero", "single", "double"]
+questions = [
+  'In which month does the peak occur? ',
+  'In which month does the valley occur?',
+  'During which periods are the observed counts dense?',
+  'During which periods are few or no counts observed?'
+]
 
 # 生成符合标准正态分布的数据
 def randomNormalDistribution():
@@ -74,35 +91,52 @@ def generateOriginData(peak):
     unique_set = set(arr)
     result_list = list(unique_set)
 
+
     return  result_list,mean
 
 
 def generateData(peak,denseNum,emptyNum):
     arr,mean=generateOriginData(peak)
+    emptyAns=[]
+    # print(f"mean的值是：{mean}")
+    # for value in mean:
+    #     print(f"peak出现在第{value/24.0/30}个月")
 
     Means=[]
     if len(mean)==0:
-        Means.append(random.randint(0,8760))
-        Means.append(random.randint(0,8760))
+        Means.append(random.randint(0,4000))
+        Means.append(random.randint(4700,8760))
     elif len(mean)==1:
-        Means.append(random.randint(mean[0]-800,mean[0]))
-        Means.append(random.randint(mean[0],mean[0]+800))
+        Means.append(random.randint(mean[0]-800,mean[0]-400))
+        Means.append(random.randint(mean[0]+400,mean[0]+800))
     else:
         randidx=random.randint(0,1)
         Means.append(random.randint(mean[randidx]-800,mean[randidx]+800))
         Means.append(random.randint(mean[1-randidx]-800,mean[1-randidx]+800))
 
+    # if denseNum==0:
+    #     idx=0
+    #     for i in range(12):
+    #         now=idx
+    #         idx+=720+random.randint(0,1)*24
+    #         temp=[x for x in arr if x>=now and x<idx]
+    #         arr=[x for x in arr if x<now or x>=idx]
+    #         step=(idx-now)//len(temp)
+    #         for i in range(len(temp)):
+    #             arr.append(now+(i+1)*step+random.randint(0,step-1))
+
     for i in range(denseNum):
         denseMean=Means[0]
         del Means[0]
         print(f"dense中心是：{denseMean/24.0/30}")
+        # print(f"dense出现在第{int(denseMean/24/30+1)}个月的第{int(denseMean/24/30*4)-int(denseMean/720)*4+1}部分")
         # for j in range(50):
         #     value=getNumberInNormalDistribution(denseMean,80)
         #     if value  not in arr:
         #         arr.append(value)
         #     # arr.append(value)
-        for j in range(61):
-            value=denseMean-90+j*3
+        for j in range(51):
+            value=denseMean-75+j*3
             if value not in arr:
                 arr.append(value)
     
@@ -110,22 +144,45 @@ def generateData(peak,denseNum,emptyNum):
         temp=arr
         emptyMean=Means[0]
         del Means[0]
-        print(f"empty中心是：{emptyMean/24.0/30}")
-        arr=[x for x in temp if x < emptyMean-120 or x > emptyMean+120]
-        num=len(temp)-len(arr)
-        step=480//num
+        # print(f"empty中心是：{emptyMean/24.0/30}")
+        print(f"empty出现在第{int(emptyMean/24/30+1)}个月的第{int(emptyMean/24/30*4)-int(emptyMean/720)*4+1}部分")
+        emptyAns.append(f"{int(emptyMean/24/30+1)}-{int(emptyMean/24/30*4)-int(emptyMean/720)*4+1}")
+        arr=[x for x in temp if x < emptyMean-105 or x > emptyMean+105]
+        num=(len(temp)-len(arr))
+        step=510//num
         idx=emptyMean//720*720
+        now=1
         for i in range(num):
-            if idx+(i+1)*step<emptyMean-120:
-                value=idx+(i+1)*step
+            if idx+(i+1)*step<emptyMean-105:
+                value=idx+(i+1)*step+random.randint(0,step)
                 if value not in arr:
                     arr.append(value)
             else:
-                now=1
-                value=emptyMean+120+now*step
+                value=emptyMean+105+now*step+random.randint(0,step)
                 now+=1
                 if value not in arr:
-                    arr.append(value)        
+                    arr.append(value)
+            
+        # idx=emptyMean//720*720
+        # # if emptyMean-idx<105:
+        # #     emptyMean=idx+105
+        # print(f"empty出现在第{int(emptyMean/24/30+1)}个月的第{int(emptyMean/24/30*4)-int(emptyMean/720)*4+1}部分")
+        # arr=[x for x in temp if x < idx or x > idx+720]
+        # num=len(temp)-len(arr)
+        # now=1
+        # step=510//num+1
+        # for i in range(num):
+        #     if idx+(i+1)*step<emptyMean-105:
+        #         value=idx+(i+1)*step+random.randint(0,step-1)
+        #         if value not in arr:
+        #             arr.append(value)
+        #         # arr.append(value)
+        #     elif emptyMean+105+now*step<idx+720:
+        #         value=emptyMean+105+now*step+random.randint(0,step-1)
+        #         now+=1
+        #         if value not in arr:
+        #             arr.append(value)
+        # # # print(list(set(arr)-set(arr1)))
 
 
 
@@ -134,8 +191,9 @@ def generateData(peak,denseNum,emptyNum):
     temp_arr=[x for x in arr if 0<=x<8760]
     result_list=sorted(temp_arr)
 
-    return result_list
-    
+    return result_list,mean,emptyAns
+
+
 def generateDataFile(peak,denseNum,emptyNum):
     timeScale1=[]
     for i in range(48):
@@ -161,7 +219,8 @@ def generateDataFile(peak,denseNum,emptyNum):
             "time":[]
         })
     
-    arr=generateData(peak,denseNum,emptyNum)
+    arr,mean,emptyAns=generateData(peak,denseNum,emptyNum)
+
 
     quartermonth=0
     halfmonth=0
@@ -190,12 +249,174 @@ def generateDataFile(peak,denseNum,emptyNum):
             monthTotal+=daysOfMonth[month]
             month+=1
         timeScale3[month]["time"].append(val-monthTotal*24)
+
+    max_density=[]
+    for i in range(12):
+        if len(timeScale3[i]["time"])>=2:
+            kde = gaussian_kde(timeScale3[i]["time"])
+            temp_density = kde(np.linspace(0, timeScale3[i]["timeRange"], 2048))   
+            density=[x*len(timeScale3[i]["time"]) for x in temp_density]
+            max_density.append({
+            "max_value":np.max(density),
+            "max_idx":np.argmax(density)
+            })
+        else:
+            max_density.append({
+            "max_value":0,
+            "max_idx":0
+            })
+        
+    max_dict = max(max_density, key=lambda x: x["max_value"])
+    max_idx=max_dict["max_idx"]
+    idx = max_density.index(max_dict)
     
+    print(f"dense在{idx+1}月的{int(max_idx/2049*4)+1}部分")
+    print(f"dense在{idx+1}月的{max_idx/2049*4}部分")
+    
+    
+    # 为了找波谷，提前将每个月的总time数存储到一个数组中
+    Totaltime=[]
+    for i in range(12):
+        Totaltime.append(len(timeScale3[i]["time"]))
+
+    
+    # peak出现的月份答案(无峰答案为-1)
+    peak_months=[int(value/720+1) for value in mean]
+    print(peak_months)
+    peak_month=-1
+    if len(mean)!=0:
+        for i, month in enumerate(peak_months):
+            max_len = max(len(timeScale3[month-2]["time"]), len(timeScale3[month-1]["time"]), len(timeScale3[month]["time"]))
+            if max_len == len(timeScale3[month-2]["time"]):
+                peak_months[i] = month - 1
+            elif max_len == len(timeScale3[month-1]["time"]):
+                peak_months[i] = month
+            else:
+                peak_months[i] = month + 1
+        peak_months.sort()
+        peak_month=peak_months[0]
+        if len(mean)==2 and len(timeScale3[peak_months[0]-1]["time"])<len(timeScale3[peak_months[1]-1]["time"]):
+            peak_month=peak_months[1]
+    
+    print(peak_months)
+    print(Totaltime)
+    # 波谷出现的月份答案（无峰跟单峰为0）
+    valley_month=-1
+    if len(mean)==2:
+        min_value=min(Totaltime[peak_months[0]-1:peak_months[1]])
+        valley_month=Totaltime.index(min_value)+1
+
+    print(f"peak出现在第{peak_month}个月")
+    print(f"valley出现在第{valley_month}个月")
+
+
+
     data={
         "timeScale1":timeScale1,
         "timeScale2":timeScale2,
         "timeScale3":timeScale3
     }
+    
+    
+    #正确答案
+    questionAns=[str(peak_month),str(valley_month),f"{idx+1}-{int(max_idx/2049*4)+1}"]
+    if emptyNum==0:
+        questionAns.append("111-111")
+    else:
+        questionAns.append(emptyAns[random.randint(0,emptyNum-1)])
+    
+    print(questionAns)
+    #options包含四个问题对应的选项，每个问题的option中有六项
+    options=[]
+    #每个问题对应的答案的索引
+    answerIdx=[]
+
+    #前两个问题
+    for i in range(2):
+        option=[]
+        for j in range(4):
+            while True:
+                random_value=random.randint(1,12)
+                if str(random_value)!=questionAns[i] and str(random_value) not in option:
+                    break
+            option.append(str(random_value))
+        option.extend(["no","not sure"])
+        if int(questionAns[i])!=-1:
+            option[random.randint(0,3)]=questionAns[i]
+        options.append(option)
+        if int(questionAns[i])==-1:
+            answerIdx.append(4)
+        else:
+            answerIdx.append(option.index(questionAns[i]))
+    
+    
+    #第三个问题
+    option=[]
+    for j in range(4):
+        first,last=questionAns[2].split('-')
+        while True:
+            random_value1=random.randint(1,12)
+            random_value2=random.randint(1,4)
+            if (str(random_value1)!=first or str(random_value2)!=last) and f"{random_value1}-{random_value2}" not in option:
+                break
+        option.append(f"{random_value1}-{random_value2}")
+    option.extend(["no","not sure"])
+    if denseNum!=0:
+        option[random.randint(0,3)]=questionAns[2]
+    options.append(option)
+    if denseNum==0:        
+        answerIdx.append(4)
+    else:
+        answerIdx.append(option.index(questionAns[2]))
+
+    #第四个问题
+    option=[]
+    if emptyNum!=2:
+        first,last=questionAns[3].split('-')
+        for j in range(4):
+            while True:
+                random_value1=random.randint(1,12)
+                random_value2=random.randint(1,4)
+                if (str(random_value1)!=first or str(random_value2)!=last) and f"{random_value1}-{random_value2}" not in option:
+                    break
+            option.append(f"{random_value1}-{random_value2}")
+        option.extend(["no","not sure"])
+        if emptyNum==1:
+            option[random.randint(0,3)]=questionAns[3]
+    else:
+        first1,last1=emptyAns[0].split('-')
+        first2,last2=emptyAns[1].split('-')
+        for j in range(4):
+            while True:
+                random_value1=random.randint(1,12)
+                random_value2=random.randint(1,4)
+                if (str(random_value1)!=first1 or str(random_value2)!=last1) and (str(random_value1)!=first2 or str(random_value2)!=last2) and f"{random_value1}-{random_value2}" not in option:
+                    break
+            option.append(f"{random_value1}-{random_value2}")
+        option.extend(["no","not sure"])
+        option[random.randint(0,3)]=questionAns[3]
+    options.append(option)
+    if emptyNum==0:        
+        answerIdx.append(4)
+    else:
+        answerIdx.append(option.index(questionAns[3]))      
+
+
+    problemId=f"{peak_string[peak]}_{denseNum}_{emptyNum}"
+    originData=[]
+    for i in range(4):
+        originData.append({
+            "data":data,
+            "problemId":problemId,
+            "question":questions[i],
+            "questionId":i,
+            "options":options[i],
+            "answerIdx":answerIdx[i]
+        })
+    
+    print(f"正确答案：{questionAns}")
+    print(f"选项：{options}")
+    print(f"正确索引：{answerIdx}")
 
     # 将数据转换为 JSON 格式字符串
     json_data = json.dumps(data, indent=4)
@@ -212,15 +433,29 @@ def generateDataFile(peak,denseNum,emptyNum):
     except Exception as e:
         print("写入出错了:", e)
     
+
+        # 将数据转换为 JSON 格式字符串
+    json_originData = json.dumps(originData, indent=4)
+    
+    # 拼接文件路径
+    file_name1 = f"{peak_string[peak]}_{denseNum}_{emptyNum}.json"
+    file_path1 = os.path.join("trial_data/lab1/exercise", file_name1)
+    
+    try:
+        # 将 JSON 数据写入文件
+        with open(file_path1, "w") as file:
+            file.write(json_originData)
+        print("文件写入成功")
+    except Exception as e:
+        print("写入出错了:", e)
+
     
 #     # 指定要输出的文件路径
 #     file_path = "data.json"
 
 #     # 将数据写入 JSON 文件
 #     with open(file_path, 'w') as json_file:
-#         json.dump(data, json_file, indent=4)
+#         json.dump(originData, json_file, indent=4)
 
 # generateDataFile(1,1,1)
-
-
 
--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from typing import List
 from fastapi import FastAPI
 import os
 import json
 from GenerateData import generateDataFile
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,39 +14,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class BasicInfo(BaseModel):
-    name: str
-    gender: str
-    age: int
-    major: str
-    contact: str
-class TimeScale(BaseModel):
-    largeScale:int
-    timeRange:int
-    time:List[int]
-class DataSet(BaseModel):
-    timeScale1:TimeScale
-    timeScale2:TimeScale
-    timeScale3:TimeScale
-class Problem(BaseModel):
-    # data: DataSet
-    question: str
-    label: str
-    questionId:int
-    rightAnswer:str
-    answer:str
-    spendTime:int
-    peak:int
-    dense:int
-    empty:int
-class ProblemInfo(BaseModel):
-    session:int
-    problems:List[Problem]
-class OneInfo(BaseModel):
-    problemInfo:List[ProblemInfo]
-    info: BasicInfo
-    completeTime: int
-
 peak_string = ["zero", "single", "double"]
 
 @app.get('/data/get')
@@ -57,7 +22,7 @@ async def get_data(peak: int, dense: int, empty: int):
         file_path = os.path.join(os.path.dirname(__file__), f"data/{peak_string[peak]}_{dense}_{empty}.json")
         with open(file_path, "r") as file:
             data = json.load(file)
-        return {"data": data}
+        return {"peak": peak, "dense": dense, "empty": empty, "data": data}
     except Exception as e:
         return {"error": f"Failed to read file: {str(e)}"}
 
@@ -72,27 +37,25 @@ async def get_sample_data(name):
         return {"error": f"Failed to read file: {str(e)}"}
 
 @app.get('/data/generate')
-async def generate_and_send_data(peak: int, dense: int, empty: int, answer: int):
+async def generate_and_send_data(peak: int, dense: int, empty: int):
     try:
         generated_data = generateDataFile(peak, dense, empty)
-        file_path = os.path.join(os.path.dirname(__file__), f"data/{peak_string[peak]}_{dense}_{empty}.json")
-        with open(file_path, "r") as file:
-            newData = json.load(file)
-        if answer==0:
-            newData["answer"]=[]
-        return {"message": "Successfully generated data", 
-                "generated_data": newData}
-
+        return {"message": "Successfully generated data", "generated_data": generated_data}
     except Exception as e:
         return {"error": f"Failed to generate data: {str(e)}"}
+    
 
-@app.post('/experiment/submit')
-async def save_experiment_data(info: OneInfo):
+@app.get('/experiment/get')
+async def generate_trail_problem(labIdx: int,type: int):
     try:
-        print(info)
-        return {"message": "Successfully saved experiment data"}
+        doc=["exercise","formal"]
+        file_path = os.path.join(os.path.dirname(__file__), f"trial_data/lab{labIdx}/{doc[type]}/single_1_1.json")
+        with open(file_path, "r") as file:
+            data = json.load(file)
+        return {"data": data}
     except Exception as e:
-        return {"error": f"Failed to save experiment data: {str(e)}"}
+        return {"error": f"Failed to read file: {str(e)}"}
+
 
 if __name__ == '__main__':
     import uvicorn
